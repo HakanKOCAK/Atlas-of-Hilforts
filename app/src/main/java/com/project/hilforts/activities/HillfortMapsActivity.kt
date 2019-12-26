1e8df2c93d2f52aaa6f6914f9611fc9e9b8a9f20
Hakan KOCAK <hakankocak@Hakans-MacBook-Pro.local>
2019-12-26 16:38:27 +0300

    Showing all Hillforts on the Map

--- app/src/main/java/com/project/hilforts/activities/HillfortMapsActivity.kt
+++ app/src/main/java/com/project/hilforts/activities/HillfortMapsActivity.kt
@@ -3,21 +3,72 @@ package com.project.hilforts.activities
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.project.hilforts.R
+import com.project.hilforts.main.MainApp
 
 import kotlinx.android.synthetic.main.activity_hillfort_maps.*
+import kotlinx.android.synthetic.main.content_hillfort_maps.*
 
-class HillfortMapsActivity : AppCompatActivity() {
+class HillfortMapsActivity : AppCompatActivity(), GoogleMap.OnMarkerClickListener {
+
+    lateinit var map: GoogleMap
+    lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort_maps)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        app = application as MainApp
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync {
+            map = it
+            configureMap()
         }
     }
 
+    override fun onMarkerClick(marker: Marker): Boolean {
+        currentTitle.text = marker.title
+        return false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    fun configureMap() {
+        map.uiSettings.setZoomControlsEnabled(true)
+        app.users.getUserHillforts(app.loggedInUserEmail).forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.title).position(loc)
+            map.addMarker(options).tag = it.id
+            map.setOnMarkerClickListener(this)
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+        }
+    }
 }
